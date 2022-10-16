cf5c6e14d9be9ea537405385024cd62c7b6034d8
Ella-1 <es9897288@gmail.com>
2022-10-16 10:26:25 -0700

    puppet config

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,20 @@
+# Install a package using Puppet
+
+exec { 'pip3 install flask':
+  command => '/usr/bin/apt-get pip3 install flask'
+}
+
+exec { 'pip3 install puppet-lint':
+  command => '/usr/bin/pip3 install flask -v 2.1.0'
+}
+
+package { 'flask':
+  ensure => 'installed',
+  before => Exec['pip3 install flask']
+}
+
+package { 'puppet-lint':
+  ensure  => 'installed',
+  before  => Exec['pip3 install puppet-lint'],
+  require => Package['flask']
+}
