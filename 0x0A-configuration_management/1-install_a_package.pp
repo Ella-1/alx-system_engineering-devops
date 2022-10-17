f72ff13e9bec1f004cbc9c58bf86f8b897de6e7d
Ella-1 <es9897288@gmail.com>
2022-10-17 08:53:35 -0700

    installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,6 @@
-# Install a package using Puppet
-
-exec { 'pip3 install flask':
-  command => '/usr/bin/pip3 install flask'
-}
-
-exec { 'pip3 install puppet-lint':
-  command => '/usr/bin/pip3 install flask -v 2.1.0'
-}
-
-package { 'flask':
-  ensure => 'installed',
-  before => Exec['pip3 install flask']
-}
-
-package { 'puppet-lint':
-  ensure  => 'installed',
-  before  => Exec['install puppet-lint'],
-  require => Package['flask']
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
